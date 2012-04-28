103772dc92ce0ebfe2194de71a8ed0fbdff03313
Andreas Mueller <am@0x7.ch>
2012-04-28 02:57:42 +0200

    decode and print ID file

--- ehealth-card.py
+++ ehealth-card.py
@@ -13,6 +13,7 @@ Communicate with Swiss eHealth cards as specified by eCH-0064.
 import sys
 import itertools
 from optparse import OptionParser
+import smartcard
 from smartcard.System import readers
 
 # master file
@@ -67,31 +68,127 @@ EF = {
 	'PrK.X509':                [ 0xDF, 0x02, 0x00, 0x17]
 }
 
+def l2s(l):
+	return "".join(map(lambda x: chr(x), l)).decode('utf-8')
+
+
+class SmartCardCommunication:
+	"""
+	Handles smard card communication with ISO 7816-4.
+	"""
+	def __init__(self, reader, verbosity):
+		self.reader = reader
+		self.connection = reader.createConnection()
+		self.verbosity = verbosity
+		try:
+			self.connection.connect()
+		except smartcard.exceptions.cardconnectionexception, e:
+			print e.message
+			sys.exit(1)
+
+	def select_file(self, file_id):
+		SELECT = [0x00, 0xa4, 0x00, 0x00, len(file_id)]
+		data, sw1, sw2 = self.connection.transmit(SELECT + file_id)
+		if self.verbosity > 0:
+			print "SELECT returned sw1 = %x, sw2 = %x" % (sw1, sw2)
+		if sw1 != 0x90:
+			sys.stderr.write("SELECT failed - exiting\n")
+			sys.exit(1)
+
+	def read_binary(self, length):
+		READ_BINARY = [0x00, 0xb0, 0x00, 0x00, 0x00, 0x00, length]
+		data, sw1, sw2 = self.connection.transmit(READ_BINARY)
+		if self.verbosity > 0:
+			print "READ BINARY returned sw1 = %x, sw2 = %x" % (sw1, sw2)
+		if sw1 != 0x90:
+			sys.stderr.write("READ BINARY failed - exiting\n")
+			sys.exit(1)
+		return data
+
+
+class HealthCard:
+
+	def __init__(self, reader, verbosity):
+		self.scc = SmartCardCommunication(reader, verbosity)
+		self.verbosity = verbosity
+		# TODO check ATR and make sure we have a health-card
+
+
+	def decode_id(self, data):
+		output = {}
+		offset = 0
+		if options.verbosity > 1:
+			print "decoding data:"
+			print [hex(x) for x in data]
+		if data[0] != 0x65:
+			sys.stderr.write("Warning: expected 0x65 as first byte of EF_ID data, but got %x\n" % data[0])
+		offset += 2
+		assert data[offset] == 0x80 # name tag
+		offset += 1
+		name_length = data[offset]
+		offset += 1
+		name = l2s(data[offset:offset+name_length])
+		output['family_name'] = name.split(',')[0].strip()
+		output['given_name'] = name.split(',')[1].strip()
+		offset += name_length
+		assert data[offset] == 0x82 # birth date tag
+		offset += 1
+		birthdate_length = data[offset]
+		assert birthdate_length == 8
+		offset += 1
+		birthdate = l2s(data[offset:offset+birthdate_length])
+		output['data_of_birth'] = (int(birthdate[0:4]), int(birthdate[4:6]), int(birthdate[6:8]))
+		offset += birthdate_length
+		assert data[offset] == 0x83 # insurance number tag
+		offset += 1
+		insurance_number_length = data[offset]
+		offset += 1
+		output['insurance_number'] = l2s(data[offset:offset+insurance_number_length])
+		offset += insurance_number_length
+		assert data[offset] == 0x84 # sex tag
+		offset += 1
+		sex_length = data[offset]
+		assert sex_length == 1
+		offset += 1
+		sex = data[offset]
+		SEX = {0: 'unknown', 1: 'male', 2: 'female', 9: 'not applicable'}
+		output['sex'] = SEX[sex]
+
+		return output
+
+	def print_id(self):
+		self.scc.select_file(EF['ID'])
+		data = self.decode_id(self.scc.read_binary(84))
+		print "Name:                  " + data['given_name'] + " " + data['family_name']
+		print "Date of birth (y-m-d): %d-%d-%d" % data['data_of_birth']
+		print "Insurance number:      " + data['insurance_number']
+		print "Sex:                   " + data['sex']
+
+
 if __name__ == "__main__":
 	parser = OptionParser(usage="%prog <cmd>", version="%prog 0.1")
 	parser.add_option("-r", "--reader", type="int", dest="reader", help="use reader number N", metavar="N")
 	parser.add_option("-l", "--list-readers", dest="list_readers", action="store_true", help="list available readers")
-	parser.add_option("-f", "--file", dest="filename", help="write report to FILE", metavar="FILE")
-	parser.add_option("-v", "--verbosity", action="count", dest="verbose", default=0, help="verbose output [default: %default]")
+	parser.add_option("-i", "--print-id", dest="print_id", action="store_true", help="read, decode and print EF_ID")
+	parser.add_option("-v", "--verbose", action="count", dest="verbosity", default=0, help="verbose output [default: %default]")
 	(options, args) = parser.parse_args()
+
+	smartcard_readers = readers()
 
 	if options.list_readers:
 		c = itertools.count()
-		for reader in readers():
-			print "%s: $s" % (c.next(), reader)
+		for reader in smartcard_readers:
+			print "%s: %s" % (c.next(), reader)
 		sys.exit(0)
 
 	if options.reader:
+		if options.reader >= len(smartcard_readers):
+			sys.stderr.write("reader not available: %s\n" % options.reader)
+			sys.exit(1)
+		reader = smartcard_readers[options.reader]
+	else:
+		reader = smartcard_readers[0]
 
-	c = r.createConnection()
-	c.connect()
-	SELECT = [0x00, 0xa4, 0x00, 0x00, 0x02]
-	data, sw1, sw2 = c.transmit(SELECT + EF['ID'])
-	print "%x %x" % (sw1, sw2)
-	READ_BINARY = [0x00, 0xb0, 0x00, 0x00, 0x00, 0x00, 0x54]
-	data, sw1, sw2 = c.transmit(READ_BINARY)
-	print "%x %x" % (sw1, sw2)
-	name_length = data[3]
-	name = data[4:4+name_length]
-	print "".join(map(lambda x: chr(x), name)).decode('utf-8')
-
+	hc = HealthCard(reader, options.verbosity)
+	if options.print_id:
+		hc.print_id()
